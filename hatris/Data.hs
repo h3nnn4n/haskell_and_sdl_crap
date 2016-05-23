5dc12e54c7372d9568a6fdf8fec71d79865c360c
h3nnn4n <uber.renan@gmail.com>
2016-05-23 11:09:39 +0000

    Now handles events (Only SDL.Quit)

--- hatris/Data.hs
+++ hatris/Data.hs
@@ -1,23 +1,48 @@
 module Data where
 
-data Piece = Blocks [Block]
+-- World is a list of blocks(Points), an integer with the points,
+-- the current piece, its coordinates and the next piece.
+--data World = Pieces ([Point], Int, Piece, Point, Piece)
+data World = World
+    { blocks    :: [Point]
+    , score     :: Int
+    , falling   :: Piece
+    , position  :: Point
+    , nextPiece :: Piece
+    , toquit    :: Bool
+    }
 
-data Block = Point (Int, Int)
+-- A piece is made of blocks, which are 2d points
+data Piece = Piece [Point]
+data Point = Point (Int, Int)
 
-data World = Pieces [Piece]
 
+-- The screen resolution
 screenx, screeny :: Int
 screenx = 800
 screeny = 600
 
+-- Board size
 boardx, boardy :: Int
 boardx = 10
 boardy = 25
 
+-- The size of each cell
 dx, dy :: Int
 dy = div screeny (boardy+1)
 dx = dy
 
+-- Where the cell starts
+-- It calculates where to draw the grid so it can be centered on the screen
 posx, posy :: Int
 posx = (div screenx 2) - (div boardx 2)*dx
 posy = (div screeny 2) - (div boardy 2)*dy
+
+newWorld = World
+    { blocks    = []
+    , score     = 0
+    , falling   = Piece [Point (1,1)]
+    , position  = Point (1, 1)
+    , nextPiece = Piece [Point (1,1), Point (1,2)]
+    , toquit    = False
+    }
--- hatris/Main.hs
+++ hatris/Main.hs
@@ -11,6 +11,7 @@ drawGrid surface = do
         forM_ [0, dy .. dy*boardy] $ \y -> do
            rectangle surface (Rect (x+posx) (y+posy) (posx+(x+dx)) (posy+(y+dy))) (Pixel 0xFFFFFF)
 
+main :: IO ()
 main = do
     SDL.init [InitEverything]
 
@@ -18,10 +19,23 @@ main = do
     setVideoMode screenx screeny 24 [HWSurface, DoubleBuf]
     surface <- getVideoSurface
 
-    forever $ do
-        drawGrid surface
-
-        SDL.flip surface
+    game <- gameLoop surface newWorld
 
     SDL.quit
 
+gameLoop :: Surface -> World -> IO World
+gameLoop surface world = do
+    event <- pollEvent
+    case eventHandler event world of
+        w@(World { toquit = True  }) -> return w
+        w@(World { toquit = False }) -> do
+            drawGrid surface
+            SDL.flip surface
+            gameLoop surface world
+
+
+eventHandler :: Event -> World -> World
+eventHandler event world =
+   case event of
+       Quit -> world { toquit = True }
+       _    -> world
